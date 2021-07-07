23300c8212732b181ebeec0459fe6aa31fca6c16
Dnyaneshwar Dake <dnyaneshwar3d@gmail.com>
2021-07-07 18:13:04 -0400

    Add files via upload

---
+++ src/test/java/com/dnyanesh/apachecamel/ApacheCamelRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnyanesh.apachecamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamelRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
